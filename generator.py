8d425fe10c05e084cd52a44f8c9abf06bf3bd562
beeean17 <yoonsb1374@gmail.com>
2025-11-05 19:52:30 +0900

    qrcode_generator

--- generator.py
+++ generator.py
@@ -1,4 +1,12 @@
 import qrcode
 
-img = qrcode.make('https://www.seoultech.ac.kr/')
-img.save('seoultech.png')
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('https://www.seoultech.ac.kr/')
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('qrcode.png')
